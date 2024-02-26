36c920d926d3dd509cf00505de8d488828703ad3
Mykhailo Holub <mishataube2005@gmail.com>
2024-02-26 15:00:45 +0200

    risky_function implemented

--- Lab_1/functions.py
+++ Lab_1/functions.py
@@ -1,2 +1,10 @@
+import random
+
 def dumb_function(a,b):
     return 'oogaa-booga'
+
+def risky_function():
+    if random.random < 0.0001:
+        return 1/0
+    else:
+        return "It's your lucky day!!!"
