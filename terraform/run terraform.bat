55e8766af8e2b025112b2db944c4ae8c414a584c
Mykhailo Holub <mishataube2005@gmail.com>
2024-06-05 20:37:56 +0300

    Update run terraform.bat

--- terraform/run terraform.bat
+++ terraform/run terraform.bat
@@ -4,12 +4,3 @@ terraform validate
 pause
 terraform apply
 pause
-awslocal s3api list-buckets
-pause
-awslocal s3api list-objects --bucket input-bucket
-awslocal s3api list-objects --bucket output-bucket
-pause
-awslocal s3api put-object --bucket input-bucket --key image.png --body image.png
-pause
-awslocal s3api list-objects --bucket input-bucket
-awslocal s3api list-objects --bucket output-bucket
