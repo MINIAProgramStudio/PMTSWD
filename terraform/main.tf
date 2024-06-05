1096358a9affd55cbb73cf566c1d32230ac8b5cd
Mykhailo Holub <mishataube2005@gmail.com>
2024-06-05 11:13:28 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,12 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+
+  endpoints {
+        lambda     = "http://localhost:4566"
+        iam        = "http://localhost:4566"
+        s3         = "http://s3.localhost.localstack.cloud:4566"
+    }
 }
 
 resource "aws_s3_bucket" "input-bucket" {
