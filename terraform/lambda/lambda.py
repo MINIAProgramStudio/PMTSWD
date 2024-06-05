3de51ea6027d695ebe8cbba19849e75dde089c99
Mykhailo Holub <mishataube2005@gmail.com>
2024-06-05 20:39:10 +0300

    Update lambda.py

--- terraform/lambda/lambda.py
+++ terraform/lambda/lambda.py
@@ -3,15 +3,24 @@ import boto3
 INPUT_BUCKET_NAME = "input-bucket"
 OUTPUT_BUCKET_NAME = "output-bucket"
 
+
 def lambda_handler(event, context):
     aws_s3 = boto3.resource("s3")
 
-    input_bucket = aws_s3.Bucket(INPUT_BUCKET_NAME)
+    if 'Records' not in event:
+        print("Event does not contain records.")
+        return
 
-    for object in input_bucket.objects:
+    for record in event['Records']:
+        object_key = record['s3']['object']['key']
+
         source = {
             "Bucket": INPUT_BUCKET_NAME,
-            "Key": object.key
+            "Key": object_key
         }
 
-        aws_s3.meta.client.copy(source, OUTPUT_BUCKET_NAME, object.key)
+        try:
+            aws_s3.meta.client.copy(source, OUTPUT_BUCKET_NAME, object_key)
+            print(f"Copied {object_key} to {OUTPUT_BUCKET_NAME}")
+        except Exception as e:
+            print(f"Error copying {object_key}: {str(e)}")
